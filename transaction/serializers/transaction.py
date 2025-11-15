11d89aec708eb5775a2eb1422d83c0f59efd3bde
mh bakhshipour <mh.bakhshipour@yahoo.com>
2025-11-15 18:31:27 +0330

    feat: Add read_only_fields to TransactionSerializer for better data integrity

--- transaction/serializers/transaction.py
+++ transaction/serializers/transaction.py
@@ -9,3 +9,4 @@ class TransactionSerializer(BaseModelserializer):
     class Meta:
         model = Transaction
         fields = ["vendor", "customer", "amount", "created_at", "updated_at"]
+        read_only_fields = ["created_at", "updated_at"]
