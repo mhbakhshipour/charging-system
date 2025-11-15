93824a4a1d43561c6c8e94889413e93d6654ac41
mh bakhshipour <mh.bakhshipour@yahoo.com>
2025-11-15 18:54:50 +0330

    fix: Remove empty permission_classes from TransactionViewSet for cleaner code

--- transaction/views/transaction.py
+++ transaction/views/transaction.py
@@ -8,7 +8,6 @@ class TransactionViewSet(BaseModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     search_fields = ["vendor_name", "customer_phone_number"]
-    permission_classes = []
 
     def perform_create(self, serializer):
         serializer.save()
