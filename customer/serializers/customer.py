5d2ab868c6ba6128006a438c69fef5426e2e54c3
mh bakhshipour <mh.bakhshipour@yahoo.com>
2025-11-15 18:28:08 +0330

    feat: Add current_balance field to CustomerSerializer

--- customer/serializers/customer.py
+++ customer/serializers/customer.py
@@ -8,4 +8,5 @@ from customer.models.customer import Customer
 class CustomerSerializer(BaseModelserializer):
     class Meta:
         model = Customer
-        fields = ["phone_number", "created_at", "updated_at"]
+        fields = ["phone_number", "current_balance", "created_at", "updated_at"]
+        read_only_fields = ["created_at", "updated_at"]
