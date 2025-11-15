61feb11dbb778547e1d68fc2e2f06225df4859d7
mh bakhshipour <mh.bakhshipour@yahoo.com>
2025-11-15 21:30:21 +0330

    feat: Enhance TransactionService to handle customer transactions and update balances accordingly

--- transaction/serializers/transaction.py
+++ transaction/serializers/transaction.py
@@ -3,6 +3,7 @@ from charging_system_b2b.utils.serializers.base_model_serializer import (
 )
 
 from transaction.models import Transaction
+from transaction.services import TransactionService
 
 
 class TransactionSerializer(BaseModelserializer):
@@ -10,3 +11,6 @@ class TransactionSerializer(BaseModelserializer):
         model = Transaction
         fields = ["vendor", "customer", "amount", "created_at", "updated_at"]
         read_only_fields = ["created_at", "updated_at"]
+
+    def create(self, validated_data):
+        return TransactionService.add_transaction(**validated_data)
--- transaction/services/transaction.py
+++ transaction/services/transaction.py
@@ -1,4 +1,4 @@
-from django.db import transaction
+from django.db import transaction, models
 
 from transaction.models import Transaction
 from transaction.tasks import update_current_balance_task
@@ -11,12 +11,25 @@ class TransactionService:
     @staticmethod
     @transaction.atomic
     def add_transaction(vendor: Vendor, amount: int, customer: Customer = None):
-        new_transaction = Transaction.objects.create(
-            vendor=vendor,
-            customer=customer,
-            amount=amount,
-        )
-
-        update_current_balance_task.delay(new_transaction.id)
-
-        return new_transaction
+        if customer is None:
+            locked_vendor = Vendor.objects.select_for_update().get(pk=vendor.pk)
+            new_transaction = Transaction.objects.create(
+                vendor=locked_vendor,
+                customer=None,
+                amount=amount,
+            )
+            Vendor.objects.filter(pk=locked_vendor.pk).update(
+                current_balance=models.F("current_balance") + amount
+            )
+            return new_transaction
+        else:
+            locked_customer = Customer.objects.select_for_update().get(pk=customer.pk)
+            new_transaction = Transaction.objects.create(
+                vendor=vendor,
+                customer=locked_customer,
+                amount=amount,
+            )
+            Customer.objects.filter(pk=locked_customer.pk).update(
+                current_balance=models.F("current_balance") + amount
+            )
+            return new_transaction
