2de394ac01f2f170a69764159fb7c7b10ddf639e
mh bakhshipour <mh.bakhshipour@yahoo.com>
2025-11-15 19:14:22 +0330

    fix: Add error handling in increase_credit method for improved response on ValueError

--- customer/views/customer.py
+++ customer/views/customer.py
@@ -10,6 +10,9 @@ def increase_credit(request):
     serializer = IncreaseCustomerCreditSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
-    CustomerService.increase_credit(**serializer.validated_data)
+    try:
+        CustomerService.increase_credit(**serializer.validated_data)
+    except ValueError as e:
+        return Response({"message": str(e)}, status=400)
 
     return Response({"data": serializer.data})
