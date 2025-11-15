1e09858c1a7f95406c4caf8cb210c0b4963d04ee
mh bakhshipour <mh.bakhshipour@yahoo.com>
2025-11-15 21:32:46 +0330

    refactor: Improve increase_credit method to use locked vendor for balance checks and transactions

--- customer/services/customer.py
+++ customer/services/customer.py
@@ -11,17 +11,18 @@ class CustomerService:
     @staticmethod
     @transaction.atomic
     def increase_credit(vendor: Vendor, amount: int, customer: Customer):
-        if not vendor.current_balance >= amount:
+        locked_vendor = Vendor.objects.select_for_update().get(pk=vendor.pk)
+        if locked_vendor.current_balance < amount:
             raise ValueError(
                 "Vendor does not have enough balance to increase customer credit."
             )
 
-        vendor_transaction = TransactionService.add_transaction(
-            vendor=vendor,
+        TransactionService.add_transaction(
+            vendor=locked_vendor,
             amount=-amount,
         )
         customer_transaction = TransactionService.add_transaction(
-            vendor=vendor,
+            vendor=locked_vendor,
             customer=customer,
             amount=amount,
         )
